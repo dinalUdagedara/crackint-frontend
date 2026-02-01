1289b3f6bfb595fa9f15b11a0a8b5270a02b7e06
Dinal Udagedara <126741968+dinalUdagedara@users.noreply.github.com>
2026-02-01 18:25:17 +0530

    style: ui refactor with height contstraints

--- components/home-dashboard/summary-section/SummaryView.tsx
+++ components/home-dashboard/summary-section/SummaryView.tsx
@@ -5,7 +5,7 @@ import SummaryCard from "./SummaryCard"
 
 export default function SummaryView() {
   return (
-    <div className="grid gap-4 md:grid-cols-3">
+    <div className="grid gap-4 md:grid-cols-3 sm:h-full h-[300px]">
       {summariesHardcoded.map((summary) => (
         <SummaryCard key={summary.title} summary={summary} />
       ))}
